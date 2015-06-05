2ada39ba3134116940fdb8e3d152fc6efd7e2cfc
shamusdougan <shamus@douganfamily.com.au>
2015-06-05 16:36:54 +1000

    Initial Build from composer

---
+++ _protected/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=irwinstockfeeds',
+    'username' => 'irwin_dbuser',
+    'password' => 'BnsSfbfXVnSH7yVE',
+    'charset' => 'utf8',
+];
