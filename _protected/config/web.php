f496dc30a636781de4b2d22336268aee4510f423
shamusdougan <shamus@douganfamily.com.au>
2015-06-06 19:52:58 +1000

    Add the auth admin section

--- _protected/config/web.php
+++ _protected/config/web.php
@@ -8,6 +8,11 @@ $config = [
     //'language' => 'sr',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+			]
+		],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) 
@@ -97,6 +102,14 @@ $config = [
         ],
         'db' => require(__DIR__ . '/db.php'),
     ],
+	'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            'admin/*',
+            '*' // add or remove allowed actions to this list
+       		 ]
+		],
+
     'params' => $params,
 ];
 
