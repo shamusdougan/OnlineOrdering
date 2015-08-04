a795bfb6be6f1bf9c553ff37f71028e3f4fd884d
shamusdougan <shamus.dougan@SHAMUS-W7.SAPIENT-TECH.local>
2015-08-05 08:51:08 +1000

    Added dashboard and login pretties

--- _protected/views/site/index.php
+++ _protected/views/site/index.php
@@ -5,61 +5,45 @@ $this->title = Yii::t('app', Yii::$app->name);
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+    <div class="sapient_home_title_wrapper">
+    	<div class='sapient_home_title'>
+    	
+    	</div>
 
-        <p class="lead">You have successfully installed Yii2 improved application template</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.freetuts.org/tutorial/view?id=6">Read our tutorial</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-3">
-                <h3>Yii documentation</h3>
+                <h3>Orders Waiting to be Submitted</h3>
+				This will hold a list of orders that are active and waiting to be submitted or actioned by each user
+                
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href="/customer-order/">Customer Orders</a></p>
             </div>
             <div class="col-lg-3">
-                <h3>Yii forum</h3>
+                <h3>Deliveries Due Today</h3>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
+                <p>This will hold a list of deliveies that are been delivered today.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-default" href="/delivery">Deliveries</a></p>
+            </div>
+             <div class="col-lg-3">
+                <h3>Recent Customers</h3>
+
+                <p>This will hold a list of recently used customers</p>
+
+                <p><a class="btn btn-default" href="/clients">Recent Customers</a></p>
             </div>
             <div class="col-lg-3">
-                <h3>Yii extensions</h3>
+                <h3>Recent Orders</h3>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
+                <p>This will hold a list of orders the have been recently entered into the system. </p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="/customer-order/">Recent Orders</a></p>
             </div>
-            <div class="col-lg-3">
-                <h3>Freetuts.org</h3>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.freetuts.org/">Freetuts.org &raquo;</a></p>
-            </div>
+           
         </div>
 
     </div>
--- _protected/views/site/login.php
+++ _protected/views/site/login.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="col-lg-5 well bs-component">
+    <div class="well bs-component">
 
         <p><?= Yii::t('app', 'Please fill out the following fields to login:') ?></p>
 
