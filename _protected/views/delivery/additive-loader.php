e01121c51e2ff37137646c41f5da5382ec1b4cdd
shamusdougan <shamus@douganfamily.com.au>
2016-06-22 10:52:12 +1000

    Changes to the Additive Sheets

--- _protected/views/delivery/additive-loader.php
+++ _protected/views/delivery/additive-loader.php
@@ -172,25 +172,31 @@ foreach($delivery->deliveryLoad as $deliveryLoadObject){
 	<div style='width: 100%; height: 520px;' >
 		<table >
 			<tr style='background-color: #c6c6c6'>
-				<td width='350px'><b>PRODUCT</B></td>
-				<td width='100px'></td><b></b></td>
-				<td width='250px'><b>WEIGHT</B></td>
+				<td width='350px'><b>Product</B></td>
+				<td width='100px' align='right'><b>%</b></td>
+				<td width='50px'><b>Weight</B></td>
+				<td width='200px' align='right'><b>Batching</B></td>
 			</tr>
 			<?
 			$boxesWidth = '150';
 			$imageWidth = min(($boxesWidth / $delivery->num_batches), 30);
+			$additiveTotalPercent = 0;
+			$additiveTotalWeight = 0;
 			foreach($delivery->customerOrder->getAdditiveIngredients() as $orderIngredient)
-				{
+				{	
+					$additivePercent = $orderIngredient->ingredient_percent;
+					$additiveWeight = ((($delivery->customerOrder->Qty_Tonnes / $delivery->num_batches) * $orderIngredient->ingredient_percent) / 100);
+					$additiveTotalPercent += $additivePercent;
+					$additiveTotalWeight += $additiveWeight;
 					echo "<tr>";
-					echo "<td><span style='font-size: 22px'>".$orderIngredient->product->Name."</spant></td>";
-					echo "<td align='right'><span style='font-size: 18px'>".number_format($orderIngredient->ingredient_percent, 3)."%</span></td>";
-					echo "<td align='right'><span style='font-size: 18px'>".number_format(((($delivery->customerOrder->Qty_Tonnes / $delivery->num_batches) * $orderIngredient->ingredient_percent) / 100), 3)."T </span>";
-					for($i=0; $i < $delivery->num_batches; $i++)
-						{
-							echo "<img src='images/tick-box.png' width='".$imageWidth."'>";
-						}
-					
-					
+						echo "<td><span style='font-size: 22px'>".$orderIngredient->product->Name."</spant></td>";
+						echo "<td align='right'><span style='font-size: 18px'>".number_format($additivePercent, 3)."%</span></td>";
+						echo "<td align='right'><span style='font-size: 18px'>".number_format($additiveWeight, 3)."T </span></td>";
+						echo "<td align='right'><span style='font-size: 18px'>";
+						for($i=0; $i < $delivery->num_batches; $i++)
+							{
+								echo "<img src='images/tick-box.png' width='".$imageWidth."'>";
+							}
 					echo "</td>";
 					echo "</tr>";
 				}
@@ -202,20 +208,31 @@ foreach($delivery->deliveryLoad as $deliveryLoadObject){
 	
 	
 	</div>
-	<div style='width: 100%; height: 100px; border: 1px solid'>Notes:<br>
-		<?= $delivery->customerOrder->Order_instructions ?>
-		
-	</div>
+	
 	<div style='width: 100%; height: 100px; margin-top: 10px'>
 		<table width='100%'>
 			<tr>
-				<td width='50%' rowspan='2' valign='top'>Mixed By:___________________________</td>
-				<td width='50%'>BULK TOTAL: <?= $delivery->customerOrder->Qty_Tonnes ?>T</td>
+				<td style='width: 25%'></td>
+				<td style='width: 25%'>ADDITIVE TOTAL:</td>
+				<td style='width: 25%'><?= number_format($additiveTotalPercent, 3) ?>%</td>
+				<td style='width: 25%'><?= number_format($additiveTotalWeight, 3) ?>T</td>
 			</tr>
 			<tr>
-				<td>ORDER TOTAL: 100.00%</td>
+				<td><img src='images/tick-box.png' width='30px'> System Flushed</td>
+				<td>ORDER TOTAL:</td>
+				<td>100.000%</td>
+				<td><?= $delivery->delivery_qty ?> </td>
 			</tr>
-			
+			<tr>
+				<td><img src='images/tick-box.png' width='30px'> Sample Taken</td>
+				<td colspan='3' style='border-bottom: 1px solid'></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td><img src='images/tick-box.png' width='30px'> Sample Inspected</td>
+				<td colspan='3' style='border-bottom: 1px solid'></td>
+				<td></td>
+			</tr>
 		</table>
 	
 	
@@ -369,7 +386,7 @@ foreach($delivery->deliveryLoad as $deliveryLoadObject){
 		<table width='100%'>
 			<tr>
 				<td width='50%' rowspan='2' valign='top'>Mixed By:___________________________</td>
-				<td width='50%'>BULK TOTAL: <?= $delivery->customerOrder->Qty_Tonnes ?>T</td>
+				<td width='50%'>BULK TOTAL: <?=  $delivery->delivery_qty ?>T </td>
 			</tr>
 			<tr>
 				<td>ORDER TOTAL: 100.00%</td>
